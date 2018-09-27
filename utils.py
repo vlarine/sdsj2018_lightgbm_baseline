f0bab35491413fd1eb5834eb1e251e3d091b6b5f
Vladimir Larin <vlarine@cudabuntu>
2018-09-27 08:56:05 +0300

    Add year and hour

--- utils.py
+++ utils.py
@@ -22,10 +22,9 @@ def transform_datetime_features(df):
     ]
     for col_name in datetime_columns:
         df[col_name] = df[col_name].apply(lambda x: parse_dt(x))
+        df['number_year_{}'.format(col_name)] = df[col_name].apply(lambda x: x.year)
         df['number_weekday_{}'.format(col_name)] = df[col_name].apply(lambda x: x.weekday())
         df['number_month_{}'.format(col_name)] = df[col_name].apply(lambda x: x.month)
         df['number_day_{}'.format(col_name)] = df[col_name].apply(lambda x: x.day)
-        #df['number_hour_{}'.format(col_name)] = df[col_name].apply(lambda x: x.hour)
-        #df['number_hour_of_week_{}'.format(col_name)] = df[col_name].apply(lambda x: x.hour + x.weekday() * 24)
-        #df['number_minute_of_day_{}'.format(col_name)] = df[col_name].apply(lambda x: x.minute + x.hour * 60)
+        df['number_hour_{}'.format(col_name)] = df[col_name].apply(lambda x: x.hour)
     return df
